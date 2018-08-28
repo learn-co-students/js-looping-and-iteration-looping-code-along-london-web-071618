70a09ceacb2446b3da085815e761255ffc3962a2
ritz-w <ritz.wu@outlook.com>
2018-08-28 13:55:11 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function printBadges(employees) {
+    for (let i=0; i < employees.length; i++) {
+      console.log(`Welcome ${employees[i]}! You are employee #${i + 1}.`);
+    }
+    return employees;
+}
+
+
+function tailsNeverFails() {
+  let accumulate = 0;
+  function coinToss () {
+    return Math.random() >= 0.5;
+  }
+  while (coinToss()) {
+    accumulate ++;
+  }
+  return `You got ${accumulate} tails in a row!`;
+}
